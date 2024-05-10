723ebe8d2f3ac4382f0adcfe17dc18b401961d9f
vander <dmtttboy@gmail.com>
2024-05-10 11:36:58 +0500

    new update

--- ttr.py
+++ ttr.py
@@ -3,25 +3,25 @@ import streamlit as st
 import matplotlib.pyplot as plt
 
 # Загружаем данные
-data_kz = pd.read_excel('kz_2014.xlsx')
-data_kz1 = pd.read_excel('kz_2015.xlsx')
-data_kz2 = pd.read_excel('kz_2016.xlsx')
-data_kz3 = pd.read_excel('kz_2017.xlsx')
+data_kz = pd.read_excel('./333/kz_2014.xlsx')
+data_kz1 = pd.read_excel('./333/kz_2015.xlsx')
+data_kz2 = pd.read_excel('./333/kz_2016.xlsx')
+data_kz3 = pd.read_excel('./333/kz_2017.xlsx')
 
-data_kgz = pd.read_excel('kgz_2014.xlsx')
-data_kgz1 = pd.read_excel('kgz_2015.xlsx')
-data_kgz2 = pd.read_excel('kgz_2016.xlsx')
-data_kgz3 = pd.read_excel('kgz_2017.xlsx')
+data_kgz = pd.read_excel('./333/kgz_2014.xlsx')
+data_kgz1 = pd.read_excel('./333/kgz_2015.xlsx')
+data_kgz2 = pd.read_excel('./333/kgz_2016.xlsx')
+data_kgz3 = pd.read_excel('./333/kgz_2017.xlsx')
 
-data_tjk = pd.read_excel('tjk_2014.xlsx')
-data_tjk1 = pd.read_excel('tjk_2015.xlsx')
-data_tjk2 = pd.read_excel('tjk_2016.xlsx')
+data_tjk = pd.read_excel('./333/tjk_2014.xlsx')
+data_tjk1 = pd.read_excel('./333/tjk_2015.xlsx')
+data_tjk2 = pd.read_excel('./333/tjk_2016.xlsx')
 data_tjk3 = pd.read_excel('tjk_2017.xlsx')
 
-data_uzb = pd.read_excel('uzb_2014.xlsx')
-data_uzb1 = pd.read_excel('uzb_2015.xlsx')
-data_uzb2 = pd.read_excel('uzb_2016.xlsx')
-data_uzb3 = pd.read_excel('uzb_2017.xlsx')
+data_uzb = pd.read_excel('./333/uzb_2014.xlsx')
+data_uzb1 = pd.read_excel('./333/uzb_2015.xlsx')
+data_uzb2 = pd.read_excel('./333/uzb_2016.xlsx')
+data_uzb3 = pd.read_excel('./333/uzb_2017.xlsx')
 
 # Определяем функции для построения графиков
 def plot_kz():
