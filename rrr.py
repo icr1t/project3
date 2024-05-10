931d76fcf4fa3fdb81c5cdd39005f84985c850cb
vander <dmtttboy@gmail.com>
2024-05-10 12:22:11 +0500

    123 izmena

--- rrr.py
+++ rrr.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import streamlit as st
-import altair as alt
+import matplotlib.pyplot as plt
+import numpy as np
+from streamlit_echarts import st_echarts
 
 # Загружаем данные
 data_kz = pd.read_excel('./333/kz_2014.xlsx')
@@ -23,12 +25,14 @@ data_uzb1 = pd.read_excel('./333/uzb_2015.xlsx')
 data_uzb2 = pd.read_excel('./333/uzb_2016.xlsx')
 data_uzb3 = pd.read_excel('./333/uzb_2017.xlsx')
 
+
 st.sidebar.header("Выберите страну")
 kaz_button = st.sidebar.button('Казахстан')
 uzb_button = st.sidebar.button('Узбекистан')
 tjk_button = st.sidebar.button('Таджикистан')
 kgz_button = st.sidebar.button('Кыргызстан')
 all_countries_button = st.sidebar.button('Общий график')
+bar_chart_button = st.sidebar.button('Установить стиль одиночного бара')
 
 F_ad_Prob_Mod_Sev_kaz_values = [0.0737473506983265, 0.044529239425859325, 0.07208697980276833, 0.09025550050680399]
 F_ad_Prob_Mod_Sev_uzb_values = [0.09872602667454446, 0.12482079148104783, 0.1033934827101725, 0.16342414956949367]
@@ -36,47 +40,48 @@ F_ad_Prob_Mod_Sev_tjk_values = [0.13234311608076732, 0.11086727498562164, 0.1959
 F_ad_Prob_Mod_Sev_kgz_values = [0.1875365079274166, 0.21059007745097164, 0.19581301002148638, 0.19449654750600165]
 years = [2014, 2015, 2016, 2017]
 
-@st.cache
-def plot_country_graph_altair(country, values, years):
-    data = pd.DataFrame({
-        'Year': years,
-        'Probability': values,
-    })
-    chart = alt.Chart(data).mark_line().encode(
-        x='Year',
-        y='Probability'
-    ).properties(
-        width=500,
-        height=300,
-        title=f'{country}'
-    )
-    st.altair_chart(chart)
+
+def plot_country_graph(country, values, years):
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.plot(years, values, marker='o', linestyle='-')
+    ax.set_title(country)
+    ax.set_xticks(years)
+    ax.set_yticks(np.arange(0, 0.31, 0.05))
+    ax.grid(True)
+    st.pyplot(fig)
+
 
 if kaz_button:
-    plot_country_graph_altair('Казахстан', F_ad_Prob_Mod_Sev_kaz_values, years)
+    plot_country_graph('Казахстан', F_ad_Prob_Mod_Sev_kaz_values, years)
 
 elif uzb_button:
-    plot_country_graph_altair('Узбекистан', F_ad_Prob_Mod_Sev_uzb_values, years)
+    plot_country_graph('Узбекистан', F_ad_Prob_Mod_Sev_uzb_values, years)
 
 elif tjk_button:
-    plot_country_graph_altair('Таджикистан', F_ad_Prob_Mod_Sev_tjk_values, years)
+    plot_country_graph('Таджикистан', F_ad_Prob_Mod_Sev_tjk_values, years)
 
 elif kgz_button:
-    plot_country_graph_altair('Кыргызстан', F_ad_Prob_Mod_Sev_kgz_values, years)
+    plot_country_graph('Кыргызстан', F_ad_Prob_Mod_Sev_kgz_values, years)
 
 elif all_countries_button:
-    data = pd.DataFrame({
-        'Year': years * 4,
-        'Probability': F_ad_Prob_Mod_Sev_kaz_values + F_ad_Prob_Mod_Sev_uzb_values + F_ad_Prob_Mod_Sev_tjk_values + F_ad_Prob_Mod_Sev_kgz_values,
-        'Country': ['Казахстан'] * 4 + ['Узбекистан'] * 4 + ['Таджикистан'] * 4 + ['Кыргызстан'] * 4,
-    })
-    chart = alt.Chart(data).mark_line().encode(
-        x='Year',
-        y='Probability',
-        color='Country',
-    ).properties(
-        width=500,
-        height=300,
-        title='Probability of Moderate Severity in Central Asia'
-    )
-    st.altair_chart(chart)
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.plot(years, F_ad_Prob_Mod_Sev_kaz_values, marker='o', linestyle='-', label='Казахстан')
+    ax.plot(years, F_ad_Prob_Mod_Sev_uzb_values, marker='o', linestyle='-', label='Узбекистан')
+    ax.plot(years, F_ad_Prob_Mod_Sev_kgz_values, marker='o', linestyle='-', label='Кыргызстан')
+    ax.plot(years, F_ad_Prob_Mod_Sev_tjk_values, marker='o', linestyle='-', label='Таджикистан')
+    ax.set_title('Центральная Азия')
+    ax.set_xticks(years)
+    ax.set_yticks(np.arange(0, 0.3, 0.05))
+    ax.legend()
+    ax.grid(True)
+    st.pyplot(fig)
+
+elif bar_chart_button:
+    options = {
+        "xAxis": {"type": "category", "data": ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]},
+        "yAxis": {"type": "value"},
+        "series": [
+            {"data": [120, {"value": 200, "itemStyle": {"color": "#a90000"}}, 150, 80, 70, 110, 130], "type": "bar"}
+        ],
+    }
+    st_echarts(options=options, height="400px")
